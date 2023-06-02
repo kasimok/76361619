ebbf1d3962b75fcf91de9b76a1f41f8a8182bb28
0x67 <evilisnj@gmail.com>
2023-06-02 14:03:31 +0800

    Initial Commit

---
+++ 76361619/_6361619App.swift
@@ -0,0 +1,17 @@
+//
+//  _6361619App.swift
+//  76361619
+//
+//  Created by 0x67 on 2023-06-02.
+//
+
+import SwiftUI
+
+@main
+struct _6361619App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
